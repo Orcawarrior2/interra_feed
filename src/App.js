c1482b9fa70355c8d358837288b1d75c733806d7
Justin Mitchell <jpmitchell@wpi.edu>
2021-05-11 10:56:16 -0400

    Added Zoo Tycoon Screenshots

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+const IOT_RULE_API_URL = "https://zj3phaj6dg.execute-api.us-east-1.amazonaws.com/Prod/publish";
+
+function publishMessage() {
+  let xhr; //Will hold an XMLHTTPRequest object
+
+  //Send Request Over HTTP
+  xhr = new XMLHttpRequest();
+  xhr.open("POST", IOT_RULE_API_URL, true); //Initialize SYNCHRONOUS HTTP Request
+  //xhr.send('{"message":"Hello world!"}');
+  xhr.send("");
+  //Check if the Request is Done
+  switch (xhr.status) {
+    case 200: //Upon success, call the processResponse function above
+      console.log("XML HTTP Response Text: " + xhr.responseText);
+      break;
+    case 400: //Upon failure, alert user unable to process request
+      alert("Unable to process request");
+      console.log("XML HTTP Response Text: " + xhr.responseText);
+      break;
+    default:
+      alert("An unknown error has occurred");
+      break;
+  }
+}
+
 function App() {
   return (
     <div className="App">
@@ -17,6 +42,9 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={e => publishMessage()}>
+          Donate here!
+        </button>
       </header>
     </div>
   );
